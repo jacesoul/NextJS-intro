20d1686ecb7e635ee88ac56f067df2cce67dec3b
jacesoul <seolju90@gmail.com>
2022-07-01 14:30:45 +0900

    Custom App

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps}></Component>
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
+}
