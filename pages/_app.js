3fe880931e71ea6aa5fcdfb4a024a5ca2b58eb10
jacesoul <seolju90@gmail.com>
2022-07-01 14:59:18 +0900

    Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps}></Component>
       <style jsx global>{`
         a {
           color: white;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
