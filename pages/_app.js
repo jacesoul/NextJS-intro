f46d68f9c67855affbcd5b58c841ec0a8282669f
jacesoul <seolju90@gmail.com>
2022-07-05 00:48:28 +0900

    Fetching Data

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps}></Component>
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
     </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "e234c8f082fd3b21c327e654e30ec72d";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home"></Seo>
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
